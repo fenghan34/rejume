105a00da7c638c587d46fed95ee2c5aea5181e42
Han Feng <fenghan770@gmail.com>
2025-07-09 10:14:35 +0800

    chore: update dashboard loading

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,9 +1,9 @@
-import { Spinner } from '@/components/icons'
+import React from 'react'
 
 export default function DashboardLoading() {
   return (
-    <div className="p-6">
-      <Spinner className="size-6 animate-spin" />
+    <div className="fixed top-0 left-0 right-0">
+      <div className="h-1 bg-purple-600 animate-dashboard-progress" />
     </div>
   )
 }
